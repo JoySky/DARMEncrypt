22a5b137287c789f801a4513425876bc6b2a8239
JoySky <joysky@outlook.com>
2015-05-19 16:04:50 +0800

    计算注册码
    wince程序计算注册码

---
+++ DARMEncrypt/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace DARMEncrypt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBuild = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMachineCode = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listBoxEncryption = new System.Windows.Forms.ListBox();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.btnAutoEncrypt = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnBuild
+            // 
+            this.btnBuild.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBuild.Location = new System.Drawing.Point(91, 389);
+            this.btnBuild.Name = "btnBuild";
+            this.btnBuild.Size = new System.Drawing.Size(75, 23);
+            this.btnBuild.TabIndex = 0;
+            this.btnBuild.Text = "生成文件";
+            this.btnBuild.UseVisualStyleBackColor = true;
+            this.btnBuild.Click += new System.EventHandler(this.btnBuild_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.Location = new System.Drawing.Point(253, 389);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "关闭";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "机器码：";
+            // 
+            // txtMachineCode
+            // 
+            this.txtMachineCode.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtMachineCode.Location = new System.Drawing.Point(71, 9);
+            this.txtMachineCode.Name = "txtMachineCode";
+            this.txtMachineCode.Size = new System.Drawing.Size(176, 21);
+            this.txtMachineCode.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.listBoxEncryption);
+            this.groupBox1.Location = new System.Drawing.Point(12, 50);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(316, 333);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "加密文件";
+            // 
+            // listBoxEncryption
+            // 
+            this.listBoxEncryption.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBoxEncryption.FormattingEnabled = true;
+            this.listBoxEncryption.ItemHeight = 12;
+            this.listBoxEncryption.Location = new System.Drawing.Point(6, 20);
+            this.listBoxEncryption.Name = "listBoxEncryption";
+            this.listBoxEncryption.Size = new System.Drawing.Size(304, 304);
+            this.listBoxEncryption.TabIndex = 1;
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnEncrypt.Location = new System.Drawing.Point(253, 9);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnEncrypt.TabIndex = 5;
+            this.btnEncrypt.Text = "手动加密";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClear.Location = new System.Drawing.Point(172, 389);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "清空";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "配置文件（*.ini）|*.ini";
+            // 
+            // btnAutoEncrypt
+            // 
+            this.btnAutoEncrypt.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAutoEncrypt.Location = new System.Drawing.Point(10, 389);
+            this.btnAutoEncrypt.Name = "btnAutoEncrypt";
+            this.btnAutoEncrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnAutoEncrypt.TabIndex = 7;
+            this.btnAutoEncrypt.Text = "自动加密";
+            this.btnAutoEncrypt.UseVisualStyleBackColor = true;
+            this.btnAutoEncrypt.Click += new System.EventHandler(this.btnAutoEncrypt_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(12, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(287, 12);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "注意：请将机器码中间的中横线（-）去掉之后再加密";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(338, 420);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnAutoEncrypt);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtMachineCode);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnBuild);
+            this.MinimumSize = new System.Drawing.Size(354, 458);
+            this.Name = "Form1";
+            this.Text = "DARM程序加密";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBuild;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMachineCode;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button btnAutoEncrypt;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListBox listBoxEncryption;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ DARMEncrypt/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace DARMEncrypt
+{
+    public partial class Form1 : Form
+    {
+        private static byte[] Keys = { 0x12, 0x34, 0x56, 0x78, 0x90, 0xAB, 0xCD, 0xEF };
+        private string encryptStr;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            int temp = listBoxEncryption.Items.Count;
+            if (temp!=0)
+            {
+                if (DialogResult.Yes == MessageBox.Show("您是否要放弃该文件？", "警告", MessageBoxButtons.YesNo))
+                {
+                    this.Close();
+                }
+                else
+                {
+                    return;
+                }
+            }
+            else
+            {
+                this.Close();
+            }
+            
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            string strCode = txtMachineCode.Text;
+            string strKey = "DARMYFZX";
+            if (strCode==String.Empty)
+            {
+                MessageBox.Show("请填写机器码！", "警告");
+                return;
+            }
+            encryptStr=EncryptDES(strCode,strKey);
+            listBoxEncryption.Items.Add(encryptStr);
+        }
+        private void btnAutoEncrypt_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            string path = openFileDialog1.FileName;
+            if (path == string.Empty)
+            {
+                return;
+            }
+            List<string> lstCode = new List<string>();
+            StreamReader objStreamReader = new StreamReader(path, Encoding.Default);
+            string str = objStreamReader.ReadLine();
+
+            while (str != null)
+            {
+                lstCode.Add(str);
+                str = objStreamReader.ReadLine();
+            }
+            objStreamReader.Close();
+            string strKey = "DARMYFZX";
+            List<string> lstEncry = new List<string>();
+            for (int i = 0; i < lstCode.Count;i++ )
+            {
+                lstEncry.Add(EncryptDES(lstCode[i], strKey));
+                listBoxEncryption.Items.Add(lstEncry[i]);
+            }
+
+        }
+        private void btnBuild_Click(object sender, EventArgs e)
+        {
+            int temp = listBoxEncryption.Items.Count;
+            if (temp == 0)
+            {
+                MessageBox.Show("没有生成注册码，不能为您生成注册文件！","警告");
+                return;
+            }
+            saveFileDialog1.ShowDialog();
+            string path = saveFileDialog1.FileName;
+            if (path==string.Empty)
+            {
+                return;
+            }
+            StreamWriter objStreamWriter = new StreamWriter(path, false);
+            
+            for (int i = 0; i < listBoxEncryption.Items.Count;i++ )
+            {
+                objStreamWriter.WriteLine(listBoxEncryption.Items[i].ToString());
+            }
+            
+            objStreamWriter.Close();
+            MessageBox.Show("文件生成完毕，请替换原有配置文件！","提示");
+
+        }
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            listBoxEncryption.Items.Clear();
+            txtMachineCode.Text = "";
+        }
+        /// <summary>
+        /// DES加密方法
+        /// </summary>
+        /// <param name="encryptString">待加密字符串</param>
+        /// <param name="encryptKey">加密密钥，要求为8位</param>
+        /// <returns>加密成功则返回加密后的字符串，否则返回原字符串</returns>
+        public string EncryptDES(string encryptString,string encryptKey)
+        {
+            try
+            {
+                byte[] rgbKey = Encoding.UTF8.GetBytes(encryptKey.Substring(0, 8));
+                byte[] rgbIV = Keys;
+                byte[] inputByteArray = Encoding.UTF8.GetBytes(encryptString);
+                DESCryptoServiceProvider desCSP=new DESCryptoServiceProvider();
+                MemoryStream mStream = new MemoryStream();
+                CryptoStream cStream = new CryptoStream(mStream, desCSP.CreateEncryptor(rgbKey, rgbIV), CryptoStreamMode.Write);
+                cStream.Write(inputByteArray, 0, inputByteArray.Length);
+                cStream.FlushFinalBlock();
+                return Convert.ToBase64String(mStream.ToArray());
+            }
+            catch 
+            {
+                return encryptString;
+            }
+        }
+
+        
+
+        
+    }
+}
